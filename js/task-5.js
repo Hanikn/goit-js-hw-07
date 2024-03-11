dea0dca39cd1b567115ccf3bcc0004f7a15c0967
Hanikn <hanikn20@gmail.com>
2024-03-11 21:14:37 +0200

    HW-07

---
+++ js/task-5.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`
+}
+
+const body_element = document.querySelector('body')
+const button_element = document.querySelector('.change-color')
+const color_element = document.querySelector('.color')
+
+button_element.addEventListener('click', (event) => {
+    body_element.style.backgroundColor = getRandomHexColor()
+    color_element.textContent = body_element.style.backgroundColor
+})
